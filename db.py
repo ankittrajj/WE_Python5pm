cd084cb66b3e8e6de2396b35e4a243c2e7656548
Ankit Raj <ankittrajj1996@gmail.com>
2022-07-23 18:24:51 +0530

    Add files via upload

---
+++ db.py
@@ -0,0 +1,20 @@
+import mysql.connector as c
+con = c.connect(host = 'localhost',
+                user = 'root',
+                passwd = 'Ankit@8285',
+                database = 'python')
+
+cursor = con.cursor()
+# if con.is_connected():
+#     print("connected!!")
+
+
+#name,age & marks --> user input!!
+name = input("Enter the name")
+age = int(input("enter the age"))
+marks = float (input("Enter your marks"))
+
+query = "insert into user values('{}',{},{})".format(name,age,marks)
+cursor.execute(query)
+con.commit()
+print("data entered successfully!!")

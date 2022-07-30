2be1e1bddd258144466c67aeb967bc64bbefeca5
Ankit Raj <ankittrajj1996@gmail.com>
2022-07-30 18:09:26 +0530

    Add files via upload

--- db.py
+++ db.py
@@ -2,19 +2,27 @@ import mysql.connector as c
 con = c.connect(host = 'localhost',
                 user = 'root',
                 passwd = 'Ankit@8285',
-                database = 'python')
+                database = 'project')
 
 cursor = con.cursor()
+
 # if con.is_connected():
-#     print("connected!!")
+#     print("connection successfully!!")
 
 
-#name,age & marks --> user input!!
-name = input("Enter the name")
-age = int(input("enter the age"))
-marks = float (input("Enter your marks"))
+choice = input("1.Open A/c\n2.Cash Withdrawl\n3.Cash deposit\n4.Account Details\n5.Exit")
 
-query = "insert into user values('{}',{},{})".format(name,age,marks)
-cursor.execute(query)
-con.commit()
-print("data entered successfully!!")
+if choice == '1':
+    name= input("entr the name")
+    age = int(input("entr the age"))
+    number = int(input("entr the number"))
+    query =  "insert into user values('{}',{},{})".format(name,age,number)
+    cursor.execute(query)
+    con.commit()
+    print("Data eneterd successfully!!")
+
+elif choice == '2':
+    name = input("Enter the name")
+    number = int(input("enter the amount"))
+
+    query = 
